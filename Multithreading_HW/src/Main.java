99c97a77fc25c628e188f0ea62464609427aed86
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2018-09-26 21:37:41 +0300

    Javalock

--- Multithreading_HW/src/Main.java
+++ Multithreading_HW/src/Main.java
@@ -1,26 +1,45 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
 
     public static void main(String[] args) {
 
         StringBuilder str = new StringBuilder();
-
+        ReentrantLock locker = new ReentrantLock();
 
         Thread t1 = new Thread(new Runnable()
         {
 
             @Override
             public void run() {
-                for (int i =0;i<1000;i++)
-                    str.append("Thread1 here ");
+                locker.lock();
+                try {
+                    for (int i = 0; i < 1000; i++)
+                        str.append("Thread1 here ");
+                } catch (Exception e) {
+                    throw e;
+                }
+                finally {
+                    locker.unlock();
+                }
             }
         });
 
         Thread t2 = new Thread(new Runnable()
         {
+
             @Override
             public void run() {
-                for (int i =0;i<1000;i++)
-                    str.append("Thread2 here ");
+                locker.lock();
+                try {
+                    for (int i = 0; i < 1000; i++)
+                        str.append("Thread2 here ");
+                } catch (Exception e) {
+                    throw e;
+                }
+                finally {
+                    locker.unlock();
+                }
             }
         });
         t1.start();
