20eccc6af3fbf28388ba52fe71834326c4d0f7d5
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2018-10-03 14:59:20 +0300

    Cosmetic fixes

--- Blur_image/src/Filter.java
+++ Blur_image/src/Filter.java
@@ -13,15 +13,15 @@ public class Filter {
 
     public Filter(@NotNull BufferedImage src) {
         this.src = src;
-        this.target=new BufferedImage(src.getWidth(), src.getHeight(), src.getType());
+        this.target = new BufferedImage(src.getWidth(), src.getHeight(), src.getType());
     }
 
-    public void process (int threadsNumber, Mode mode) throws InterruptedException {
-        lineCounter=0;
+    public void process(int threadsNumber, Mode mode) throws InterruptedException {
+        lineCounter = 0;
         Runnable run = new Runnable() {
             @Override
             public void run() {
-                while (lineCounter < maxIndex){
+                while (lineCounter < maxIndex) {
                     processLine(mode, lineCounter++);
                 }
             }
@@ -34,11 +34,11 @@ public class Filter {
         }
 
         Thread[] threads = new Thread[threadsNumber];
-        for (int i=0;i<threadsNumber;i++) {
-            threads[i]= new Thread(run);
+        for (int i = 0; i < threadsNumber; i++) {
+            threads[i] = new Thread(run);
             threads[i].start();
         }
-        for (int i=0;i<threadsNumber;i++){
+        for (int i = 0; i < threadsNumber; i++) {
             threads[i].join();
         }
 
@@ -46,11 +46,11 @@ public class Filter {
 
     private void processLine(Mode mode, int index) {
         if (mode == Mode.HORIZONTAL_PROCESSING) {
-            for (int i=0; i<src.getWidth(); i++) {
-                processPixel(i,index);
+            for (int i = 0; i < src.getWidth(); i++) {
+                processPixel(i, index);
             }
         } else {
-            for (int i=0; i<src.getHeight(); i++) {
+            for (int i = 0; i < src.getHeight(); i++) {
                 processPixel(index, i);
             }
         }
@@ -59,28 +59,29 @@ public class Filter {
     private void processPixel(int x, int y) {
         int pixel_cntr = 0;
         int[] colors = new int[3];
-        for (int i=-RADIUS; i<RADIUS; i++){
-            for (int j=-RADIUS; j<RADIUS; j++){
-                if (x+i<src.getWidth() && x+i >= 0 && y+j<src.getHeight() && y+j>=0) {
+        for (int i = - RADIUS; i < RADIUS; i++) {
+            for (int j = - RADIUS; j < RADIUS; j++) {
+                if (x + i < src.getWidth() && x + i >= 0 && y + j < src.getHeight() && y + j >= 0) {
                     pixel_cntr++;
-                    Color color = new Color(src.getRGB(x+i, y+j));
+                    Color color = new Color(src.getRGB(x + i, y + j));
                     int red = color.getRed();
                     int green = color.getGreen();
                     int blue = color.getBlue();
-                    colors[0]+=red;
-                    colors[1]+=green;
-                    colors[2]+=blue;
+                    colors[0] += red;
+                    colors[1] += green;
+                    colors[2] += blue;
                 }
             }
         }
-        int new_red = colors[0]/pixel_cntr;
-        int new_green=colors[1]/pixel_cntr;
-        int new_blue = colors[2]/pixel_cntr;
+        int new_red = colors[0] / pixel_cntr;
+        int new_green = colors[1] / pixel_cntr;
+        int new_blue = colors[2] / pixel_cntr;
         int new_color = getIntFromColor(new_red, new_green, new_blue);
-        target.setRGB(x,y,new_color);
+        target.setRGB(x, y, new_color);
 
     }
-    public int getIntFromColor(int red, int green, int blue){
+
+    public int getIntFromColor(int red, int green, int blue) {
         red = (red << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
         green = (green << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
         blue = blue & 0x000000FF; //Mask out anything not blue.
--- Blur_image/src/Main.java
+++ Blur_image/src/Main.java
@@ -7,23 +7,23 @@ import java.io.IOException;
 
 public class Main {
 
-    static String file_name="cat";
-    static String file_format="jpg";
+    static String file_name = "cat";
+    static String file_format = "jpg";
     static String path = "res/";
 
 
     public static void main(String[] args) throws IOException {
         BufferedImage src = null;
-        src = ImageIO.read(new File(path+file_name+"."+file_format));
+        src = ImageIO.read(new File(path + file_name + "." + file_format));
 
-        Filter filter=new Filter(src);
+        Filter filter = new Filter(src);
         try {
-            filter.process(8, Mode.HORIZONTAL_PROCESSING);
+            filter.process(2, Mode.HORIZONTAL_PROCESSING);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         BufferedImage target = filter.getTarget();
-        ImageIO.write(target, file_format, new File(path+file_name+"_output."+file_format) );
+        ImageIO.write(target, file_format, new File(path + file_name + "_output." + file_format));
         System.out.println("image created");
 
     }
