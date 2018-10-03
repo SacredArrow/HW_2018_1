3ae74b2cc033e7af929411d0a1318aae06475f4f
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2018-10-04 00:36:24 +0300

    Tests added

---
+++ Blur_image/test/target/generated-sources/annotations/org/sample/generated/MyBenchmark_BigImage_jmhType.java
@@ -0,0 +1,4 @@
+package org.sample.generated;
+public class MyBenchmark_BigImage_jmhType extends MyBenchmark_BigImage_jmhType_B3 {
+}
+
---
+++ Blur_image/test/target/generated-sources/annotations/org/sample/generated/MyBenchmark_SmallImage_jmhType.java
@@ -0,0 +1,4 @@
+package org.sample.generated;
+public class MyBenchmark_SmallImage_jmhType extends MyBenchmark_SmallImage_jmhType_B3 {
+}
+
