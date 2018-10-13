f657f7b861b974c1e8ab7097767830a6344ae204
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2018-10-13 15:17:22 +0300

    Working 3.3

---
+++ Algos/3.3/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static int sizeOfANumber = 8;
+    public static int numberOfThreads = 4;
+    public static volatile ArrayList<Carry> prefixes;
+    static volatile Carry[] carries;
+    static volatile int countedPrefixes;
+    static int step;
+    static volatile int[] result;
+
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = "37569867";
+        String str2 = "18913675";
+        str1 = new StringBuilder(str1).reverse().toString();
+        str2 = new StringBuilder(str2).reverse().toString();
+        RunCarries[] runs = new RunCarries[numberOfThreads];
+        Thread[] threads = new Thread[numberOfThreads];
+        int[] num1 = new int[sizeOfANumber];
+        int[] num2 = new int[sizeOfANumber];
+        carries = new Carry[sizeOfANumber];
+        result = new int[sizeOfANumber];
+        prefixes = new ArrayList<>();
+        for (int i = 0; i < str1.length(); i++) {
+            num1[i] = str1.charAt(i) - '0';
+            num2[i] = str2.charAt(i) - '0';
+        }
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            step = sizeOfANumber / numberOfThreads;
+            if (i == numberOfThreads - 1) {
+                runs[i] = new RunCarries(i * step, sizeOfANumber, num1, num2, carries);
+            } else {
+                runs[i] = new RunCarries(i * step, step * (i + 1), num1, num2, carries);
+            }
+            threads[i] = new Thread(runs[i]);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i].join();
+        }
+
+//        for (int i = 0; i < Main.sizeOfANumber; i++) {
+//            System.out.print(Main.carries[i]);
+//        }
+//        System.out.println();
+        for (int i = sizeOfANumber - 1; i >= 0; i--) {
+            System.out.print(result[i]);
+        }
+//        System.out.println();
+//        for (int i = 0; i < Main.numberOfThreads; i++) {
+//            System.out.print(Main.prefixes.get(i));
+//        }
+    }
+}
---
+++ Algos/3.3/src/Tree.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+
+public class Tree {
+    Tree left;
+    Tree right;
+    Carry value;
+    int left_bound;
+    int right_bound;
+
+    public Tree(int left_bound, int right_bound) {
+        this.left_bound = left_bound;
+        this.right_bound = right_bound;
+    }
+
+    @Override
+    public String toString() {
+        if (left != null && value != null) {
+            return ("Tree{ " + left.toString() + " " + (value != null ? value.toString() : "") + " " + right.toString() + " }");
+        } else if (left != null) {
+            return ("Tree{ " + left.toString() + right.toString() + " }");
+        } else {
+            return value.toString();
+        }
+    }
+
+    public void create_tree(ArrayList<Carry> array, int left_bound, int right_bound) {
+        if (left_bound == right_bound) {
+            this.value = array.get(left_bound - 1);
+        } else {
+            int middle = left_bound + (right_bound - left_bound + 1) / 2 - 1;
+            left = new Tree(left_bound, middle);
+            right = new Tree(middle + 1, right_bound);
+            Thread t1 = new Thread(() -> left.create_tree(array, left_bound, middle));
+            Thread t2 = new Thread(() -> right.create_tree(array, middle + 1, right_bound));
+            t1.start();
+            t2.start();
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public Carry upsweep() {
+        if (value != null) {
+            return value;
+        } else {
+            class Upsweep implements Runnable {
+                Tree tree;
+                Carry return_value;
+
+                public Upsweep(Tree tree) {
+                    this.tree = tree;
+                }
+
+                @Override
+                public void run() {
+                    return_value = tree.upsweep();
+                }
+            }
+            Upsweep run1 = new Upsweep(left);
+            Upsweep run2 = new Upsweep(right);
+            Thread t1 = new Thread(run1);
+            Thread t2 = new Thread(run2);
+            t1.start();
+            t2.start();
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            value = RunCarries.getCarry_sum(run1.return_value, run2.return_value);
+            return value;
+        }
+    }
+
+    public void downsweep(Carry car) {
+        value = car;
+        if (left_bound == right_bound) {
+            Main.prefixes.set(left_bound - 1, value);
+            return;
+        }
+        Carry calculated_value = RunCarries.getCarry_sum(value, left.value);
+        Thread t1 = new Thread(() -> left.downsweep(value));
+        Thread t2 = new Thread(() -> right.downsweep(calculated_value));
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
