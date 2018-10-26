653c429902501c61cd7f08d7fa6f9cf98f59c880
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2018-10-26 13:01:57 +0300

    9 tests

---
+++ Algos/9/test/src/main/java/org/src/Main.java
@@ -0,0 +1,63 @@
+package org.src;
+import static java.lang.System.exit;
+
+public class Main {
+    public static int sizeOfAString;
+    public static int numberOfThreads = 4;
+    public static volatile int[] prefixes;
+    public static int[] intArray;
+    public static final Object sync = new Object();
+    public static int countedPrefixes = 0;
+    static int step;
+    public static void main(String[] args) {
+        String str1 = "(()))(()";
+        sizeOfAString = str1.length();
+        str1 = str1.replace('(', '1').replace(')', '0');
+        String[] arr = str1.split("");
+        intArray = new int[arr.length];
+        prefixes = new int[numberOfThreads];
+        for (int i=0;i<arr.length;i++) {
+            intArray[i] = Integer.parseInt(arr[i]);
+            if (intArray[i]==0) {
+                intArray[i]=-1;
+            }
+        }
+        Thread[] threads = new Thread[numberOfThreads];
+        step = sizeOfAString/numberOfThreads;
+        for (int i=0 ; i<numberOfThreads; i++) {
+            if (i==numberOfThreads-1) {
+                threads[i] = new Thread(new Run(i*step,sizeOfAString,i));
+            } else {
+                threads[i] = new Thread(new Run(i*step, (i+1)*step, i));
+            }
+            threads[i].start();
+        }
+        for (int i=0 ; i<numberOfThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i =0 ; i<sizeOfAString;i++) {
+            if (intArray[i]<0) {
+//                System.out.println("Mismatch brackets on " + i);
+//                exit(1);
+                return;
+            }
+        }
+        if (intArray[sizeOfAString-1] != 0) {
+//            System.out.println("Mismatch brackets in the end");
+//            exit(2);
+            return;
+        }
+
+//        System.out.println("Brackets match!");
+//        exit(0);
+
+
+
+        
+//        System.out.println(intArray.);
+    }
+}
---
+++ Algos/9/test/src/main/java/org/src/Run.java
@@ -0,0 +1,53 @@
+package org.src;
+public class Run implements Runnable {
+    int from;
+    int to;
+    int number;
+
+    public Run(int from, int to, int number) {
+        this.from = from;
+        this.to = to;
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        int sum=0;
+        for (int i=from; i<to;i++){
+            sum+=Main.intArray[i];
+        }
+
+        Main.prefixes[number] = sum;
+        synchronized (Main.sync) {
+            Main.countedPrefixes++;
+            if (Main.countedPrefixes == Main.numberOfThreads) {
+                Tree head = new Tree(1, Main.numberOfThreads);
+                head.create_tree(1, Main.numberOfThreads);
+                head.upsweep();
+                head.downsweep(0);
+                Main.countedPrefixes = 0;
+                Main.sync.notifyAll();
+            } else {
+                try {
+                    Main.sync.wait();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        int total;
+        if (from != 0) {
+            total = Main.prefixes[number];
+        } else {
+            total = 0;
+        }
+        Main.intArray[from] = total + Main.intArray[from];
+        for (int i = from + 1; i < to; i++) {
+            Main.intArray[i] = Main.intArray[i-1] + Main.intArray[i];
+        }
+
+
+
+    }
+}
